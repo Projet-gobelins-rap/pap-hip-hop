00bbdd0954ffcfe9fce91c8771c01245ea3e5c2a
Yoan Vandevelde <yoan.vandevelde.isn@gmail.com>
2022-05-01 17:55:05 +0200

    ✨ Hood page

---
+++ pages/hood/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="hood">
+    <canvas id="canvasGlobalScene" ref="canvasGlobalScene"></canvas>
+  </section>
+</template>
+
+<script lang="ts">
+import { Vue, Component, getModule, Watch } from "nuxt-property-decorator";
+import stepStore from "~/store/stepStore";
+@Component({
+  components: {
+  },
+})
+
+export default class GrenierScene extends Vue {
+  public stepStore = getModule(stepStore,this.$store)
+
+  mounted() {
+
+    console.log('Boyz in da hood');
+    
+  }
+
+}
+</script>
