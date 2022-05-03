08a4278ce46b931649fbf6a0051c30c840a965d2
Yoan Vandevelde <yoan.vandevelde.isn@gmail.com>
2022-05-03 16:48:42 +0200

    ✨Use data from prismic

---
+++ components/contentOverlays/dialog.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="dialog">
+    oui
+    <div class="dialog-content">
+      <div class="dialog-infos">
+        <picture class="dialog-infos--pic">
+          <PrismicImage
+            class="dialog-infos--img"
+            :field="content.primary.Photo"
+          />
+        </picture>
+        <p class="dialog-infos--name">{{ content.primary.Nom }}</p>
+      </div>
+      <PrismicRichText :field="content.primary.Text" />
+    </div>
+    <div
+      class="dialog-choices"
+      v-for="(item, i) in content.items"
+      :key="`slice-item-${i}`"
+    >
+      {{ item.Choix }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, getModule, Prop } from "nuxt-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class DialogComponent extends Vue {
+  @Prop({ required: true }) readonly content!: any;
+
+  mounted() {
+    console.log(this.content);
+  }
+}
+</script>
--- pages/graf/scope.vue
+++ pages/graf/scope.vue
@@ -2,24 +2,49 @@
   <section class="scope desktop">
     {{ this.graf }}
     <img class="scope-background" src="/images/graf/city-rooftop.png" alt="">
+    <DialogComponent :content="dialogs[dialogStep]"/>
   </section>
 </template>
 
 <script lang="ts">
 import { Vue, Component, getModule } from "nuxt-property-decorator";
+
+import DialogComponent from "~/components/contentOverlays/dialog.vue";
 import $socket from "~/plugins/socket.io";
 
 @Component({
-  components: {},
+  components: {
+      DialogComponent
+  },
+  async asyncData({ $prismic, error }) {
+    try{
+      const scopeContent = (await $prismic.api.getSingle('interaction-graff')).data
+      const dialogs = scopeContent?.slices1
+      return {
+        dialogs,
+        scopeContent
+      }
+    } catch (e) {
+      // Returns error page
+    //   error({ statusCode: 404, message: 'Content not found' })
+    }
+  }
 })
+
+  
 export default class GraffActivity extends Vue {
   public graf: string = "Scope";
+  public scopeContent:any
+  public dialogs:any
+  public dialogStep: number = 0
+  
   mounted() {
     console.clear();
     console.log("mounted hook on HOME page");
 
+    // console.log(this.dialogs); 
+     
     console.log($socket,'socket from plugin')
-
   } 
 }
 </script>
