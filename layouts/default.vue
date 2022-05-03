25d47698ff2c388727fa0ac20c0dde7daff470f2
naelmessaoudene <naelmessaoudene@gmail.com>
2022-05-03 19:10:47 +0200

    📱implement mobile version of AssetsManager

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,9 @@
   <div class="main">
     <navigation/>
     <h1>{{this.icon}}</h1>
+
+    <div v-if="$device.isDesktop">{{this.desktopMediasURL}}</div>
+    <div v-if="$device.isMobileOrTablet">{{this.mobileMediasURL}}</div>
     <nuxt />
   </div>
 </template>
@@ -20,8 +23,16 @@ import {IMAGE_ASSET} from "../core/enums";
   },
 
   async fetch() {
-    const desktopMedias = (await this.$prismic.api.getSingle('desktopMedias')).data
-    this.$nuxt.$emit("loadDesktopMedia", {desktopMedias})
+    if (this.$device.isMobileOrTablet){
+      console.log('movile')
+      const mobileMedias = (await this.$prismic.api.getSingle('mobileMedias')).data
+      this.$nuxt.$emit("loadMobileMedia", {mobileMedias})
+    } else {
+      console.log('DESKTOP')
+      const desktopMedias = (await this.$prismic.api.getSingle('desktopMedias')).data
+      this.$nuxt.$emit("loadDesktopMedia", {desktopMedias})
+    }
+
   },
 
 
@@ -34,6 +45,8 @@ export default class Default extends Vue {
   public desktopMedias:any
   public desktopMediasURL:[{name:string,url:string,type:number}] = []
 
+  public mobileMedias:any
+  public mobileMediasURL:[{name:string,url:string,type:number}] = []
   mounted(){
 
     this.$nuxt.$on("loadDesktopMedia",(desktopMedia)=>{
@@ -61,15 +74,48 @@ export default class Default extends Vue {
       this.initApp()
     })
 
+    this.$nuxt.$on("loadMobileMedia",(mobileMedia)=>{
+      this.mobileMedias = mobileMedia.mobileMedias.slices[0].items
+      this.mobileMedias.forEach((el)=>{
+        let mediaType
+        if (el.mediaType === 'GLTF'){
+          mediaType = 0
+        }else if (el.mediaType === 'IMAGE') {
+          mediaType = 1
+        }
+        else if (el.mediaType === 'VIDEO') {
+          mediaType = 2
+        }
+        else if (el.mediaType === 'AUDIO') {
+          mediaType = 3
+        }
+
+        this.mobileMediasURL.push({
+          name:el.mediaName[0].text,
+          url:el.mediaElement.url,
+          type:mediaType
+        })
+      })
+      this.initApp()
+    })
+
   }
 
+  // TODO :: refacto pour eviter les bouts de code dupliqués
   public async initApp() {
     if (!this.globalStore.isAppInit) {
       // new AssetManagerInitializer(null).init();
       /* We need to download all asset before init app */
-      this.desktopMediasURL.forEach(el=>{
-        AssetsManager._registerSource(el.name,el.type,el.url,null)
-      })
+      if (this.$device.isMobileOrTablet){
+        this.mobileMediasURL.forEach(el=>{
+          AssetsManager._registerSource(el.name,el.type,el.url,null)
+        })
+      }else {
+        this.desktopMediasURL.forEach(el=>{
+          AssetsManager._registerSource(el.name,el.type,el.url,null)
+        })
+      }
+
       await AssetsManager.onProgress((done, total) => {
         this.loadingProgressions = Math.floor((done / total) * 100).toString();
         console.log(this.loadingProgressions,' <---- LOADING')
@@ -87,7 +133,9 @@ export default class Default extends Vue {
   onMotionValueChanged(val:boolean) {
     if (val) {
       // INSTANCE img
-      console.log(AssetsManager.getImage(IMAGE_ASSET.STICKER))
+      if (this.$device.isDesktop){
+        console.log(AssetsManager.getImage(IMAGE_ASSET.STICKER))
+      }
     }
   }
 
