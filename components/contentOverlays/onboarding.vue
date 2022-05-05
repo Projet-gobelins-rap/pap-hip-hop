095daf7c2160671aeb593bc1c8c9f3caa5de2f21
naelmessaoudene <naelmessaoudene@gmail.com>
2022-05-05 13:02:54 +0200

    ✨ setup onboarding component

---
+++ components/contentOverlays/onboarding.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="onboarding">
+    <h1>Onboarding</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "nuxt-property-decorator";
+@Component
+export default class Onboarding extends Vue{
+
+  mounted(){
+    console.log('Onboarding component')
+  }
+}
+</script>
