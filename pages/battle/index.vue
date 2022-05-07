72303d88bd0ac95b13837d6dbf7f2c88ab99689a
naelmessaoudene <naelmessaoudene@gmail.com>
2022-05-07 17:33:33 +0200

    🚧 implement store

--- pages/battle/index.vue
+++ pages/battle/index.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="battle">
     <h1>BATTLE DESKTOP</h1>
+    <ChatComponent v-if="this.chatElementState" :content="currentChat" />
   </section>
 </template>
 
@@ -8,19 +9,51 @@
 import { Vue, Component, getModule } from "nuxt-property-decorator";
 import stepStore from "~/store/stepStore";
 import CustomButton from "~/components/buttons/button.vue";
-import $socket from "~/plugins/socket.io";
+import ChatComponent from "~/components/contentOverlays/chat.vue";
+import chatStore from "~/store/chatStore";
+import battleStore from "../../store/battleStore";
+
 @Component({
   components: {
-    CustomButton
+    CustomButton,
+    ChatComponent
+  },
+  async asyncData({ $prismic, error }) {
+    try {
+      const battleContent = (await $prismic.api.getSingle("battle"))
+        .data;
+      const battleDialog = battleContent?.slices1;
+      const battleOnboarding = battleContent?.slices2;
+      // const conversation = battleContent?.slices1;
+
+      return {
+        battleDialog,
+        battleOnboarding,
+      };
+    } catch (e) {
+      // Returns error page
+      //   error({ statusCode: 404, message: 'Content not found' })
+    }
   },
 })
 export default class battle extends Vue {
   public codeValue:number | null = null
 
   public stepStore = getModule(stepStore,this.$store)
+  public battleStore = getModule(battleStore,this.$store)
+  public chatStore = getModule(chatStore, this.$store)
+  public battleDialog:any
+  public battleOnboarding:any
+  public currentChat:any
 
   mounted() {
     console.log('BATTLE')
+    console.log(this.battleDialog,'<--- dialog battle')
+    console.log(this.battleOnboarding,'<--- onboarding battle')
+  }
+
+  get chatElementState() {
+    return this.battleStore.isChatDisplay
   }
 
 }
---
+++ store/battleStore.ts
@@ -0,0 +1,27 @@
+import {Module, Mutation, VuexModule} from "vuex-module-decorators";
+import {InteractionPoint} from "../core/config/grenier-scene/interact-points/types";
+import GrenierSceneHelper from "../core/config/grenier-scene/GrenierSceneHelper";
+
+@Module({
+  name: "battleStore",
+  namespaced: true,
+  stateFactory: true
+})
+export default class battleStore extends VuexModule {
+
+  private _isChatDisplay: boolean = false
+
+
+  @Mutation
+  public setIsChatDisplay(isDisplay: boolean) {
+    this._isChatDisplay = isDisplay
+
+    return this
+  }
+
+  // Getters
+  get isChatDisplay() {
+    return this._isChatDisplay
+  }
+
+}
