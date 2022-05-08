57765cc9f1c1a63c607c4f8b61f1e02127e6c503
naelmessaoudene <naelmessaoudene@gmail.com>
2022-05-09 00:33:11 +0200

    🚧 create choices component & setup prismic data

---
+++ components/Choice.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="choices">
+
+    <div v-for="(item, i) in content"  class="choices__item">
+      <span>{{item.content[0].text}}</span>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, getModule, Prop } from "nuxt-property-decorator";
+import CustomButton from "~/components/buttons/button.vue";
+import chatStore from "~/store/chatStore";
+
+@Component({
+  components: {
+    CustomButton,
+  },
+})
+export default class Choice extends Vue {
+  @Prop({ required: true }) readonly content!: any;
+  // public chatStore = getModule(chatStore, this.$store);
+
+  mounted() {
+    console.log(this.content,'<-- content choice')
+    this.content.forEach((element)=>{
+      console.log(element,'chouces')
+    })
+    // console.log(this.content,'<---');
+
+    // // TODO : check if empty
+    // // for (const item of this.content.items) {
+    //
+    // // }
+    //
+    // // TODO : handle click on text if no choices buttons
+    // document.querySelector(".chat-text")?.addEventListener("click", (e) => {
+    //   this.chatStore.setChatStep("next");
+    // });
+  }
+
+}
+</script>
--- pages/_mobile/battle/index.vue
+++ pages/_mobile/battle/index.vue
@@ -1,25 +1,46 @@
 <template>
   <section class="battle--mobile">
-    <h1>BATTLE MOBILE</h1>
+    <Choice :content="battlePunchRound1"></Choice>
   </section>
 </template>
 
 <script lang="ts">
 import { Vue, Component, getModule } from "nuxt-property-decorator";
-import stepStore from "~/store/stepStore";
 import CustomButton from "~/components/buttons/button.vue";
 import $socket from "~/plugins/socket.io";
+import battleStore from "../../../store/battleStore";
+import Choice from '~/components/Choice'
 @Component({
   components: {
-    CustomButton
+    CustomButton,
+    Choice
+  },
+  async asyncData({ $prismic, error }) {
+    try {
+      const battleContent = (await $prismic.api.getSingle("battle")).data;
+
+      const battlePunchRound1 = battleContent?.slices3[0].items;
+
+      // const currentChat = battleChat[0];
+      // const currentOnboarding = battleOnboarding[0];
+
+
+      return {
+        battlePunchRound1,
+      };
+    } catch (e) {
+      // Returns error page
+      //   error({ statusCode: 404, message: 'Content not found' })
+    }
   },
 })
 export default class battleMobile extends Vue {
   public codeValue:number | null = null
-
-  public stepStore = getModule(stepStore,this.$store)
-
+  public battleStore = getModule(battleStore,this.$store)
+  public battlePunchRound1: object;
   mounted() {
+
+    console.log(this.battlePunchRound1,'<-- punch round 1')
     // $socket.on("phone_connected", (user) => {
     //   alert("phone_connected");
     //   this.$router.push('/')
