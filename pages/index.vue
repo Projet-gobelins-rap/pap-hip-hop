7dccc392a10a26ca1f7671f8efc819ee2d2bb849
naelmessaoudene <naelmessaoudene@gmail.com>
2022-05-15 17:57:33 +0200

    ♻️ remove globalScene on desktop index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,22 @@
 <template>
   <section class="home">
     <nuxt-link class="home-link" to="/_mobile/phone">Mobile</nuxt-link>
-    <canvas id="canvasGlobalScene" ref="canvasGlobalScene"></canvas>
   </section>
 </template>
 
 <script lang="ts">
 import { Vue, Component, getModule } from "nuxt-property-decorator";
 import globalSceneStore from "~/store/globalSceneStore";
-// import $socket from "~/plugins/socket.io";
-import GlobalScene from "~/core/scene/GlobalScene";
-import GlobalSceneInitializer from "~/core/utils/initializers/GlobalSceneInitializer";
-//
 @Component({
   components: {},
 })
 export default class Home extends Vue {
-  public globalSceneStore = getModule(globalSceneStore, this.$store);
-
+  
   mounted() {
-    // Initialize Scene
-    new GlobalSceneInitializer({
-      canvas: this.$refs.canvasGlobalScene as HTMLCanvasElement,
-      globalSceneStore: this.globalSceneStore,
-    }).init();
-
-    console.log(GlobalScene, "global scene");
-  }
-
-  stopSceneRender() {
-    GlobalScene.context.pause();
   }
 }
 </script>
- 
+
  <style lang="scss">
 .home {
   height: 100%;
@@ -48,4 +31,3 @@ export default class Home extends Vue {
   }
 }
 </style>
- 
