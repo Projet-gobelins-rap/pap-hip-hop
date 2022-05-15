c64b917b1e71b369e6b6406bf009f29b1435cde7
Yoan Vandevelde <yoan.vandevelde.isn@gmail.com>
2022-05-15 17:25:26 +0200

    ✨graff onboarding

--- pages/_mobile/graff/bomb.vue
+++ pages/_mobile/graff/bomb.vue
@@ -4,14 +4,9 @@
       <span class="graffBomb-debug--x">0</span>
       <span class="graffBomb-debug--y">0</span>
     </div>
-    <CustomButton
-      class="graffBomb-button"
-      :text="'pssscht'"
-    />
-    <CustomButton
-      class="graffBomb-button--calibrate"
-      :text="'Calibrate'"
-    />
+    <CustomButton class="graffBomb-button" :text="'pssscht'" />
+    <CustomButton class="graffBomb-button--calibrate" :text="'Calibrate'" />
+    <Onboarding :content="currentOnboarding"></Onboarding>
   </section>
 </template>
 
@@ -20,19 +15,72 @@ import { Vue, Component, getModule, Watch } from "nuxt-property-decorator";
 import stepStore from "~/store/stepStore";
 import GraffBomb from "~/core/interactions/GraffBomb.ts";
 import CustomButton from "~/components/buttons/button.vue";
+import Onboarding from "~/components/contentOverlays/onboarding";
+import onboardingStore from "~/store/onboardingStore";
 
 @Component({
   components: {
-      CustomButton
+    CustomButton,
+    Onboarding
+  },
+  async asyncData({ $prismic, error }) {
+    try {
+      const graffContent = (await $prismic.api.getSingle("interaction-graff"))
+        .data;
+
+      console.log(graffContent);
+
+      const gameplayOnboarding = graffContent?.slices6[0].items;
+
+      // const currentChat = battleChat[0];
+      const currentOnboarding = gameplayOnboarding;
+
+      return {
+        currentOnboarding,
+      };
+    } catch (e) {
+      // Returns error page
+      //   error({ statusCode: 404, message: 'Content not found' })
+    }
   },
 })
 export default class Bomb extends Vue {
   public stepStore = getModule(stepStore, this.$store);
+  public onboardingStore = getModule(onboardingStore, this.$store);
+  public currentOnboarding: object;
   public graffBombInstance: GraffBomb;
 
   mounted() {
     this.graffBombInstance = new GraffBomb();
+    this.displayOnboarding();
+     console.log(this.currentOnboarding, "<--- current onboarrding mobile");
   }
 
+  // GETTERS
+  get onboardingStep() {
+    return this.onboardingStore.onboardingStep;
+  }
+
+  displayOnboarding() {
+    this.onboardingStore.setOnboardingDisplay(true);
+  }
+
+  hideOnboarding() {
+    this.onboardingStore.setOnboardingDisplay(false);
+  }
+  @Watch("onboardingStep", { immediate: true, deep: true })
+  setOnboardingStep(val: string) {
+    if (val) {
+      console.log(val);
+      switch (val) {
+        case "reading":
+          break;
+        case "closePopup":
+          this.hideOnboarding();
+          this.onboardingStore.setOnboardingStep("reading");
+          break;
+      }
+    }
+  }
 }
 </script>
