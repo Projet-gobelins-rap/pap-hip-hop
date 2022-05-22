3ed281bfbe9284d153cd0c21e25f9425c586f18a
Yoan Vandevelde <yoan.vandevelde.isn@gmail.com>
2022-05-22 17:29:57 +0200

    💄 title screen elements

--- pages/intro/index.vue
+++ pages/intro/index.vue
@@ -1,8 +1,12 @@
 <template>
   <section class="intro">
-      <h1>PAP'HIP HOP</h1>
-      <h2>LE MOUVEMENT OUBLIÉ</h2>
-      <CustomButton  @click.native="goToNextStep" text="Commencer l'expérience"></CustomButton>
+    <h1 class="intro-title">PAP'HIP HOP</h1>
+    <h2 class="intro-subtitle">LE MOUVEMENT OUBLIÉ</h2>
+    <CustomButton
+      class="intro-button"
+      @click.native="goToNextStep"
+      text="Commencer l'expérience"
+    ></CustomButton>
   </section>
 </template>
 
@@ -10,8 +14,8 @@
 import { Vue, Component, getModule } from "nuxt-property-decorator";
 import stepStore from "~/store/stepStore";
 import CustomButton from "~/components/buttons/button.vue";
-import {AssetsManager} from "../../core/managers";
-import {IMAGE_ASSET} from "../../core/enums";
+import { AssetsManager } from "../../core/managers";
+import { IMAGE_ASSET } from "../../core/enums";
 import $storage from "../../core/utils/Storage";
 @Component({
   components: {
@@ -19,23 +23,19 @@ import $storage from "../../core/utils/Storage";
   },
 })
 export default class Intro extends Vue {
-
-  public stepStore = getModule(stepStore,this.$store)
-  public onboardingData:any
+  public stepStore = getModule(stepStore, this.$store);
+  public onboardingData: any;
 
   mounted() {
-
-    console.log(this.onboardingData,'<- onboarding')
+    console.log(this.onboardingData, "<- onboarding");
   }
 
-  goToNextStep(){
-    this.stepStore.setIntroState(true)
-    this.$router.push('/connection')
+  goToNextStep() {
+    this.stepStore.setIntroState(true);
+    this.$router.push("/connection");
   }
-
 }
 </script>
 
 <style lang="sass" scoped>
-
 </style>
