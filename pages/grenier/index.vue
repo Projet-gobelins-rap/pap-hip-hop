a2d8cd2dbd7694bda6220f1dac2481a65678e097
naelmessaoudene <naelmessaoudene@gmail.com>
2022-06-22 17:02:03 +0200

    🚑️ fix text error on page transition

--- pages/grenier/index.vue
+++ pages/grenier/index.vue
@@ -159,7 +159,7 @@ export default class GrenierScene extends Vue {
         title.innerHTML = 'LE TIEKS'
 
         let infoContent = document.querySelector('.transitionInfo-content span') as HTMLElement
-        infoContent.innerHTML = `Un quartier ou rien ne se passe`
+        infoContent.innerHTML = `Un quartier où rien ne se passe`
 
         let tl = gsap.timeline()
         tl.fromTo(
